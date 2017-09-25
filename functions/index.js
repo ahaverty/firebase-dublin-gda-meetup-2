77b63f82bc153272d6c22563f8ef59d77916b3a1
Alan Haverty <alan@bambooapp.ie>
2017-09-25 18:52:52 +0100

    Add twilio texting function

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,19 @@
 const functions = require('firebase-functions');
+
+/**
+ * EXAMPLE 1
+ * Removing profanity and guessing languages of messages
+ * 
+ * messageLanguageTagger('This is a test!',{params: {messageId: 'testId1'}})
+ */
+
 var franc = require('franc');
 var emoji = require('node-emoji');
 var swearjar = require('swearjar');
 
 exports.messageLanguageTagger = functions.database.ref('/messages/{messageId}').onCreate(event => {
     const message = event.data.val();
+
     const language = franc(message);
     const isProfane = swearjar.profane(message);
     const cleanMessage = swearjar.censor(message);
@@ -12,9 +21,40 @@ exports.messageLanguageTagger = functions.database.ref('/messages/{messageId}').
     const preText = language && language !== 'und' ? language : emoji.get('question');
     const profanityEmoji = isProfane ? emoji.get('frowning') : "\uD83D\uDE00";
 
-    console.log(`TEST Alan: ${profanityEmoji}`);
-
     return event.data.ref.parent.parent.child('taggedTextMessages').push(`${profanityEmoji} (${preText}) ${cleanMessage}`);
 });
 
-// messageLanguageTagger('this is a test',{params: {messageId: 'testId1'}})
+
+
+
+
+
+
+/**
+ * EXAMPLE 2
+ * Texting customers with lovely deeplinks
+ * textReferralSender('0876914896', {params: {phoneNumberId: 'testPhone1'}})
+ */
+
+var twilioClient = require('twilio');
+var cats = require('cat-ascii-faces')
+
+exports.textReferralSender = functions.database.ref('/phoneNumbers/{phoneNumberId}').onCreate(event => {
+    const phoneNumber = event.data.val();
+
+    console.log(functions.config())
+
+    var accountSid = functions.config().twilio.account_sid;
+    var authToken = functions.config().twilio.auth_token;
+    var fromNumber = functions.config().twilio.from_number;
+
+    console.log(`${accountSid} and ${authToken}`)
+    var client = new twilioClient(accountSid, authToken);
+
+    return client.messages.create({
+            body: `The cat thinks you should download Bamboo https://zj5z2.app.goo.gl/XTMY    ${cats()}`,
+            to: phoneNumber,
+            from: '+353861802824'
+        })
+        .then((message) => console.log(message.sid));
+});
